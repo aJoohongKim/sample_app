89471ac2720c63336460319d722607ee51876818
Joohong Kim <joohong.kim@access-company.com>
2012-06-27 20:00:29 +0900

    Inital Import with rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '6c150bbbd3cd317385620225447d034f3062faa5d59d239c2ecefbba61fc6d16756d2ba262637796a3f8af78bf4a88e71fe994ab13fff261b8521012ab49f640'
